33cc080e3ab8e6fb30dbca76955d9f91f9fd6b8d
Ijequay Wisdom <wisdomijequay@gmail.com>
2020-03-26 21:30:52 +0100

    python task
    My first python task

---
+++ basic calculator.py
@@ -0,0 +1,8 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 22:45:29) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> from math import pi
+>>> r = float(input ("Input the radius of the circle : "))
+Input the radius of the circle : 1
+>>> print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
+The area of the circle with radius 1.0 is: 3.141592653589793
+>>> 
