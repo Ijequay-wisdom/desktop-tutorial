8bc17dccc4243ff8dabaeca32b9993488def6fd8
Ijequay Wisdom <wisdomijequay@gmail.com>
2020-03-27 23:28:20 +0100

    javascript task
    first javascript task

---
+++ javascript task 1.js
@@ -0,0 +1,9 @@
+var name ="wisdom ijequay
+var courses = ["HTML", "CSS","Java script","python"];
+var number = courses.lenght;
+console.log(number)
+for(var i=0; i<=200;i++){
+    if((i%2)==0){
+        console.log(i);
+    }
+}
